171a39332856e9a631e04c088c49136c633385f6
Basti797 <ol.se.sp@hotmail.com>
2024-11-14 10:59:46 +0100

    chat client

--- ChatClient/src/main/java/org/example/Main.java
+++ ChatClient/src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.example.chatclient.ChatClient;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String serverAddress = "localhost";
+        int port = 12345;
+
+        new ChatClient(serverAddress, port);
     }
 }
