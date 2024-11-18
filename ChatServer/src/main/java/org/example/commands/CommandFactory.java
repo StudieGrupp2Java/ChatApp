d4f7acbe174d155b564688b13996c4d7ee8b8404
[Ninis Blomerus] <[itsabunny@proton.me]>
2024-11-18 12:59:21 +0100

    Improved CommandFactory to be more scalable and flexible

--- ChatServer/src/main/java/org/example/commands/CommandFactory.java
+++ ChatServer/src/main/java/org/example/commands/CommandFactory.java
@@ -1,19 +1,24 @@
 package org.example.commands;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
 public class CommandFactory {
+    private static final Map<String, Supplier<Command>> commandMap = new HashMap<>();
+
+    static {
+        commandMap.put("login", LoginCommand::new);
+        commandMap.put("register", RegisterCommand::new);
+        commandMap.put("dm", DMCommand::new);
+        commandMap.put("changepassword", ChangePasswordCommand::new);
+    }
 
     public static Command getCommand(String commandName) {
-        switch (commandName.toLowerCase()) {
-            case "login":
-                return new LoginCommand();
-            case "register":
-                return new RegisterCommand();
-            case "dm":
-                return new DMCommand();
-            case "changepassword":
-                return new ChangePasswordCommand();
-            default:
-                throw new IllegalArgumentException("Unknown command: " + commandName);
+        Supplier<Command> commandSupplier = commandMap.get(commandName.toLowerCase());
+        if (commandSupplier != null) {
+            return commandSupplier.get();
         }
+        throw new IllegalArgumentException("Unknown command: " + commandName);
     }
 }
