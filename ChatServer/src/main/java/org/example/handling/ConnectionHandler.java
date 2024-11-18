93a7a291050a5d13e5c1086dd67124e8ea8a24d4
Basti797 <sebastian.spangberg@utb.ecutbildning.se>
2024-11-18 13:07:28 +0100

    la till filter

--- ChatServer/src/main/java/org/example/handling/ConnectionHandler.java
+++ ChatServer/src/main/java/org/example/handling/ConnectionHandler.java
@@ -34,7 +34,7 @@ public class ConnectionHandler extends Thread {
                 String incomingMessage = in.readLine();
                 System.out.println(incomingMessage);
                 main.getUserManager().getUsers().forEach(user -> {
-                    String fullMessage = String.format("[%s] %s", user.getName(), incomingMessage);
+                    String fullMessage = String.format("[%s] %s", user.getName(), main.getChatFilter().filterMessage(incomingMessage));
                     user.getHandler().sendMessage(fullMessage);
                 });
 
