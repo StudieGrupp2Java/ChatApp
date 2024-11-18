13b110380d27de2243f94d8d0d04c72cf65edc8c
[Ninis Blomerus] <[itsabunny@proton.me]>
2024-11-18 13:06:46 +0100

    Improved Command:  DRY (don't repeat yourself)

--- ChatServer/src/main/java/org/example/commands/Command.java
+++ ChatServer/src/main/java/org/example/commands/Command.java
@@ -1,12 +1,16 @@
 package org.example.commands;
 
 public abstract class Command {
+    // Template method to handle common functionality
+    public final void executeWithValidation(String[] args) {
+        validateArgs(args, getExpectedArgsCount());
+        execute(args);
+    }
 
-    // Abstrakt metod som alla kommandon måste implementera
-    public abstract void execute(String[] args);
+    protected abstract void execute(String[] args);
+    protected abstract int getExpectedArgsCount();
 
-    // Valfritt: Validera argument om det behövs
-    public void validateArgs(String[] args, int expectedArgs) {
+    protected void validateArgs(String[] args, int expectedArgs) {
         if (args.length < expectedArgs) {
             throw new IllegalArgumentException("Invalid arguments. Expected " + expectedArgs + " arguments.");
         }
