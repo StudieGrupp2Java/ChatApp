d44a18b2c153e74f531dd7c1f988626bd62605f2
[Ninis Blomerus] <[itsabunny@proton.me]>
2024-11-22 11:53:56 +0100

    init

---
+++ ChatServer/src/main/java/org/example/commands/DeleteMeCommand.java
@@ -0,0 +1,11 @@
+package org.example.commands;
+
+public class DeleteMeCommand {
+    /**
+     * Som en användare vill jag kunna radera mitt konto så att jag inte behöver ha kvar mina uppgifter
+     *
+     * Kunna radera sin användare om man vill
+     *
+     * /DeleteMe
+     */
+}
