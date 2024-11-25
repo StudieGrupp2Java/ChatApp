fca49dcd8a332ace721042eb2f44ad78be2db443
[Ninis Blomerus] <[itsabunny@proton.me]>
2024-11-25 10:35:57 +0100

    Deletes user Sends confirmation message (logs the event?)

--- ChatServer/src/main/java/org/example/commands/DeleteAccountCommand.java
+++ ChatServer/src/main/java/org/example/commands/DeleteAccountCommand.java
@@ -4,30 +4,35 @@ import org.example.ChatServer;
 import org.example.handling.ConnectionHandler;
 import org.example.users.User;
 import org.example.users.UserManager;
+import org.example.util.ChatLogs;
 
 public class DeleteMeCommand extends Command {
 
     @Override
     protected void execute(String[] args, ChatServer main, ConnectionHandler sender) {
-        // 1. Kolla om användaren är inloggad
+
         UserManager userManager = main.getUserManager();
         User user = userManager.getUser(sender.getIdentifier());
+        //ChatLogs chatLogs = new ChatLogs();
 
+        // 1. Kolla om användaren är inloggad
         if (user == null){
             sender.sendMessage("You're not logged in and can't delete your account.");
             return;
         }
+
+        // 2. Radera användaren
+        userManager.deleteUser(user.getUsername());
+
+        // 3. Skicka bekräftelsemeddelande till klienten
+        sender.sendMessage("Your account is now deleted. We're sorry to see you go!");
+
+        // 4. Eventuellt: Logga händelse för administratörsändamål
+        // ChatLogs.getChatLogs().logAction("User " + user.getUsername() + " deleted their account.");
     }
 
     @Override
     protected int getExpectedArgsCount() {
         return 0;
     }
-    /**
-     * Som en användare vill jag kunna radera mitt konto så att jag inte behöver ha kvar mina uppgifter
-     *
-     * Kunna radera sin användare om man vill
-     *
-     * /DeleteMe
-     */
 }
