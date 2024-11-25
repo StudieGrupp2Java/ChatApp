51c6949e2ccd892682ffc3ad0840520149a3436c
[Ninis Blomerus] <[itsabunny@proton.me]>
2024-11-25 09:56:38 +0100

    Checks whether user is logged in

--- ChatServer/src/main/java/org/example/commands/DeleteAccountCommand.java
+++ ChatServer/src/main/java/org/example/commands/DeleteAccountCommand.java
@@ -1,6 +1,28 @@
 package org.example.commands;
 
-public class DeleteMeCommand {
+import org.example.ChatServer;
+import org.example.handling.ConnectionHandler;
+import org.example.users.User;
+import org.example.users.UserManager;
+
+public class DeleteMeCommand extends Command {
+
+    @Override
+    protected void execute(String[] args, ChatServer main, ConnectionHandler sender) {
+        // 1. Kolla om användaren är inloggad
+        UserManager userManager = main.getUserManager();
+        User user = userManager.getUser(sender.getIdentifier());
+
+        if (user == null){
+            sender.sendMessage("You're not logged in and can't delete your account.");
+            return;
+        }
+    }
+
+    @Override
+    protected int getExpectedArgsCount() {
+        return 0;
+    }
     /**
      * Som en användare vill jag kunna radera mitt konto så att jag inte behöver ha kvar mina uppgifter
      *
