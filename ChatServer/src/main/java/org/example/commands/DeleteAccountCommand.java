671c6ba94cbd34895d21fe575c5c16b8bede48c5
[Ninis Blomerus] <[itsabunny@proton.me]>
2024-11-25 11:19:44 +0100

    minor changes

--- ChatServer/src/main/java/org/example/commands/DeleteAccountCommand.java
+++ ChatServer/src/main/java/org/example/commands/DeleteAccountCommand.java
@@ -6,7 +6,7 @@ import org.example.users.User;
 import org.example.users.UserManager;
 import org.example.util.ChatLogs;
 
-public class DeleteMeCommand extends Command {
+public class DeleteAccountCommand extends Command {
 
     @Override
     protected void execute(String[] args, ChatServer main, ConnectionHandler sender) {
@@ -15,19 +15,19 @@ public class DeleteMeCommand extends Command {
         User user = userManager.getUser(sender.getIdentifier());
         //ChatLogs chatLogs = new ChatLogs();
 
-        // 1. Kolla om användaren är inloggad
+        // Kolla om användaren är inloggad
         if (user == null){
             sender.sendMessage("You're not logged in and can't delete your account.");
             return;
         }
 
-        // 2. Radera användaren
-        userManager.deleteUser(user.getName());
+        // Radera användaren
+        userManager.removeUser(sender.getIdentifier());
 
-        // 3. Skicka bekräftelsemeddelande till klienten
+        // Skicka bekräftelsemeddelande till klienten
         sender.sendMessage("Your account is now deleted. We're sorry to see you go!");
 
-        // 4. Eventuellt: Logga händelse för administratörsändamål
+        // Eventuellt: Logga händelse för administratörsändamål
         // ChatLogs.getChatLogs().logAction("User " + user.getName() + " deleted their account.");
     }
 
