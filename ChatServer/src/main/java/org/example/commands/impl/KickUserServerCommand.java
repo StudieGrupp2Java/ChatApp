9c687559a7f0d6a9ff1465fc9ca50112bb2874a5
adrjo <adrianjohansson16@gmail.com>
2024-11-28 11:00:46 +0100

    Add KickUserServerCommand

--- ChatServer/src/main/java/org/example/commands/impl/KickUserServerCommand.java
+++ ChatServer/src/main/java/org/example/commands/impl/KickUserServerCommand.java
@@ -2,12 +2,26 @@ package org.example.commands.impl;
 
 import org.example.ChatServer;
 import org.example.commands.ServerCommand;
+import org.example.handling.ConnectionHandler;
+import org.example.users.User;
+
+import java.util.Optional;
 
 public class KickUserServerCommand extends ServerCommand {
 
     @Override
     protected void execute(String[] args, ChatServer main) {
+        String username = args[0];
 
+        final Optional<User> user = main.getUserManager().getUser(username);
+
+        user.ifPresent(value -> {
+            ConnectionHandler connection = main.getClientManager().get(value.getIdentifier());
+            if (connection != null) {
+                connection.sendMessage("Kicked by admin");
+                main.getClientManager().removeConnection(connection);
+            }
+        });
     }
 
     @Override
